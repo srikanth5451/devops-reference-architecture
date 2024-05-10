cdda72b4cb06112421a1db3b619c0e82b055f054
Your Name <your@email.com>
2024-05-10 12:38:00 +0100

    docs: Add architecture diagrams

--- infrastructure-as-code/terraform/gcp-gke/main.tf
+++ infrastructure-as-code/terraform/gcp-gke/main.tf
@@ -24,3 +24,11 @@ resource "google_container_node_pool" "dev-nodes" {
     Commit      = "git-2239"
   }
 }
+# 2025-04-07 - feat: Implement EKS cluster autoscaling
+resource "azurerm_kubernetes_cluster" "prod-cluster" {
+  initial_node_count = 2
+  tags = {
+    Environment = "prod"
+    Commit      = "git-6924"
+  }
+}

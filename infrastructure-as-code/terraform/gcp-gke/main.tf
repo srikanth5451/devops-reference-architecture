067bf96068680db710834ef1327bb03d21facfc8
Your Name <your@email.com>
2024-06-25 18:42:00 +0100

    fix(ci): Correct GitHub Actions workflow

--- infrastructure-as-code/terraform/gcp-gke/main.tf
+++ infrastructure-as-code/terraform/gcp-gke/main.tf
@@ -131,3 +131,11 @@ resource "google_container_node_pool" "canary-pool" {
     Commit      = "git-4333"
   }
 }
+# 2025-04-07 - refactor: Convert Pulumi to TypeScript
+resource "google_container_node_pool" "canary-pool" {
+  initial_node_count = 2
+  tags = {
+    Environment = "dev"
+    Commit      = "git-5940"
+  }
+}
